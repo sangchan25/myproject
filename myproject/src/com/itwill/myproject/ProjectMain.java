683e08d048e0b93e5c56513dceb41c962aa34e9a
sangchan25 <162412930+sangchan25@users.noreply.github.com>
2024-11-25 12:01:32 +0900

    GitHub에서 수정...

--- myproject/src/com/itwill/myproject/ProjectMain.java
+++ myproject/src/com/itwill/myproject/ProjectMain.java
@@ -5,6 +5,9 @@ public class ProjectMain {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!");
 		System.out.println("안녕하세요, 깃허브!");
+		System.out.println("깃허브에서 작성됨...");
+
+				   
 	}
 
 }
